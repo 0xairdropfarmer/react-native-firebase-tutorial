9a43e95833bc59d7129bf2fb4e709cbc48f61132
Krissanawat <krissanwat101@gmail.com>
2019-03-16 18:06:40 +0700

    add signup code

--- App.js
+++ App.js
@@ -2,7 +2,31 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as firebase from "firebase";
 import { Container, Item, Form, Input, Button, Label } from "native-base";
+
+var config = {
+  apiKey: "AIzaSyDFdsjQWG8IFLXmviNqSiVZMw_ADFl5tpo",
+  authDomain: "react-native-firebase-3bde9.firebaseapp.com",
+  databaseURL: "https://react-native-firebase-3bde9.firebaseio.com",
+  projectId: "react-native-firebase-3bde9",
+  storageBucket: "react-native-firebase-3bde9.appspot.com",
+  messagingSenderId: "269398778466"
+};
+firebase.initializeApp(config);
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  SignUp = (email, password) => {
+    try {
+      firebase.auth().createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error.toString(error));
+    }
+  };
   render() {
     return (
       <Container style={styles.container}>
@@ -21,6 +45,9 @@ export default class App extends React.Component {
           </Item>
           <Button full rounded style={{ marginTop: 20 }}>
             <Text>SignIn</Text>
+          </Button>
+          <Button full rounded success style={{ marginTop: 20 }}>
+            <Text>Signup</Text>
           </Button>
         </Form>
       </Container>
