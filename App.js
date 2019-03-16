0197c3d11db3ccd2bbeb159e726f64b3292d5ac0
Krissanawat <krissanwat101@gmail.com>
2019-03-16 17:43:08 +0700

    add firebase depenedency and tweak form

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as firebase from "firebase";
 import { Container, Item, Form, Input, Button, Label } from "native-base";
 export default class App extends React.Component {
   render() {
     return (
-      <Container>
+      <Container style={styles.container}>
         <Form>
           <Item floatingLabel>
             <Label>Email</Label>
@@ -12,9 +13,13 @@ export default class App extends React.Component {
           </Item>
           <Item floatingLabel>
             <Label>Password</Label>
-            <Input autoCapitalize="none" autoCorrect={false} />
+            <Input
+              secureTextEntry={true}
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
           </Item>
-          <Button full rounded>
+          <Button full rounded style={{ marginTop: 20 }}>
             <Text>SignIn</Text>
           </Button>
         </Form>
@@ -27,7 +32,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    // alignItems: "center",
+    justifyContent: "center",
+    padding: 10
   }
 });
